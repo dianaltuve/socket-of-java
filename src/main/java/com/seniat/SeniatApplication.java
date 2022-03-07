a1d6ba1337e041f1680b2b2c325aa82f895fee0e
daltuve <daltuve@puropago.com>
2022-03-07 14:14:11 -0400

    7-03-2022 creación de proyecto

---
+++ src/main/java/com/seniat/SeniatApplication.java
@@ -0,0 +1,13 @@
+package com.seniat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seniat/SeniatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
